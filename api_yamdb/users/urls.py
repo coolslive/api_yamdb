87d9874c1eff7157f99507d5792fe36a9ab84398
igor <111879295+Igoryndezp@users.noreply.github.com>
2023-03-28 23:39:44 +0300

    fix authentication

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path , include
 from rest_framework.routers import DefaultRouter
 from users.views import signup, token
 
-
 router_v1 = DefaultRouter()
 
 urlpatterns = [
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,6 +1,5 @@
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
@@ -8,29 +7,30 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from secrets import token_hex
 
 from .serializers import SignUpSerializer, ConfirmationCodeSerializer
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def signup(request):
     serializer = SignUpSerializer(data=request.data)
-    if serializer.is_valid():
-        email = request.data['email']
-        code = token_hex(16)
-        send_mail(
-            'код восстановления',
-            f'{code}',
-            'from@example.com',
-            [f'{email}'],
-            fail_silently=True,
-        )
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+    user, created = User.objects.get_or_create(
+        email=serializer.validated_data['email'],
+        username=serializer.validated_data['username'],
+    )
+    confirmation_code = default_token_generator.make_token(user)
+    send_mail(
+        subject="YaMDb registration",
+        message=f"Your confirmation code: {confirmation_code}",
+        from_email=None,
+        recipient_list=[user.email],
+    )
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(["POST"])
